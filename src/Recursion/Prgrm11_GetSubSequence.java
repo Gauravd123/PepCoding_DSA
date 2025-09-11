13c6a29bde58d9c19eb43bcc2abd2a3401cba78b
Gauravd123 <gauravdebnath1998@gmail.com>
2025-09-11 23:33:15 +0530

    Get Sub Sequence

---
+++ src/Recursion/Prgrm11_GetSubSequence.java
@@ -0,0 +1,44 @@
+package Recursion;
+
+import java.util.ArrayList;
+
+public class Prgrm11_GetSubSequence {
+
+	public static void main(String[] args) {
+		
+		
+		ArrayList<String> res = gss("abc");
+		System.out.println(res);
+	}
+
+	
+	public static ArrayList<String> gss(String str)
+	{
+		
+		if(str.length()==0)
+		{
+			ArrayList<String> bres = new ArrayList<String>();
+			bres.add("");
+			return bres;
+			
+		}
+		char ch = str.charAt(0);
+		String s = str.substring(1);
+		ArrayList<String> res = gss(s);
+		
+		ArrayList<String> mres = new ArrayList<String>();
+		
+		for(String s1: res)
+		{
+			mres.add(""+s1);
+		}
+		
+		for(String s2: res)
+		{
+			mres.add(ch+s2);
+		}
+		
+		return mres;
+		
+	}
+}
